d45de112a1d4af952d75717fef970020f0ae77d6
nathaniel-grandelis <grandelisn@gmail.com>
2021-02-19 13:43:31 -0500

    mvp y'all

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import reducer from './reducers';
 import "./index.css";
 import App from "./App";
 
+const store = createStore(reducer, applyMiddleware(thunk, logger));
 const { worker } = require('./mocks/browser');
 worker.start();
 
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-    <App />, 
+    <Provider store = {store}>
+        <App />
+    </Provider>, 
     rootElement
 );
 
